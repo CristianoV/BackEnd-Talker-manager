299c0523913dfd93d9dcda203cfa8b3c01bdc58c
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-02 19:05:47 -0300

    adicionado rota de api que responde com uma lista completa de narradores

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_request, response) => {
+  const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
+  // if (talkers) {
+  //   return response.status(HTTP_OK_STATUS).json(talkers);
+  // }
+  // return response.status(HTTP_OK_STATUS).send([]);
+    return response.status(HTTP_OK_STATUS).send(talkers);
 });
 
 app.listen(PORT, () => {
