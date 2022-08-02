8afbe8df2330260c3dec7f4234772055cc718d4a
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-02 20:51:34 -0300

    adiciona linhas de comando para abrir rota de pesquisa por id

--- index.js
+++ index.js
@@ -6,20 +6,33 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_BAD_REQUEST_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+// não remova esse endpoint, e para o avaliador funcionar
 
 app.get('/talker', (_request, response) => {
   const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
-  // if (talkers) {
-  //   return response.status(HTTP_OK_STATUS).json(talkers);
-  // }
-  // return response.status(HTTP_OK_STATUS).send([]);
-    return response.status(HTTP_OK_STATUS).send(talkers);
+  if (talkers) {
+    return response.status(HTTP_OK_STATUS).json(talkers);
+  }
+    return response.status(HTTP_OK_STATUS).json([]);
+});
+
+app.get('/talker/:id', (_request, response) => {
+  const { id } = _request.params;
+  const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf8'));
+  const talkerFindId = talkers.find((talkerId) => talkerId.id === Number(id));
+  if (!talkerFindId) {
+    return response.status(HTTP_BAD_REQUEST_STATUS).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+    return response.status(HTTP_OK_STATUS).json(talkerFindId);
 });
 
 app.listen(PORT, () => {
